508ef9d86faa36fabca51d3a82d9df9f49ff270a
nast <eha20082@gmail.com>
2025-04-25 09:49:10 +0300

    Vaatame kolm esimest rida tabelist Person

--- laused.sql
+++ laused.sql
@@ -132,7 +132,7 @@ select * from Person where Email like '_@_.com'
 
 --?
 select * from Person where Name like '[^WAS]%'
---- ?
+---  Vaatame kolm esimest rida tabelist Person
 select * from Person where (City = 'Gotham' or City = 'New York')
 and Age >= 40
 
