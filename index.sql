bea152663600396dac5db148b5f4189e0820b381
nast <eha20082@gmail.com>
2025-04-25 10:51:19 +0300

    new items

--- index.sql
+++ index.sql
@@ -87,28 +87,6 @@ insert into Employees (Id, Name, Gender, Salary, DepartmentId)
 values (10, 'Russell', 'Male', 8800, NULL)
 
 
---näitab tabelissisu, kui kasutaja sisestab gender(sugu)
-create proc spGetEmployeesByGenderAndDepartment
-@Gender nvarchar(20),
-@DepartmentId int
-as begin
-	select Name, Gender, DepartmentId from Employees where Gender = @Gender
-	and DepartmentId = @DepartmentId
-end
-
---see käsklus nõuab, et antakse Gender parameeter
-spGetEmployeesByGenderAndDepartment
--- õige variant
-spGetEmployeesByGenderAndDepartment 'Male', 1
-
---- niimoodi saab j'rjekorda muuta päringul, kui ise paned muutuja paika
-spGetEmployeesByGenderAndDepartment @DepartmentId = 1, @Gender = 'Male'
-
--- soov vaadata sp sisu
-sp_helptext spGetEmployeesByGenderAndDepartment
-
---- 5tund 17.03.2025
-
 --- kuidas muuta sp-d ja pane krüpteeringu peale, et keegi teine peale teid ei saaks muuta
 alter proc spGetEmployeesByGenderAndDepartment
 @Gender nvarchar(20),
@@ -141,168 +119,91 @@ else
 print @TotalCount
 
 
--- annab tulemuse, kus loendab ära nõuetele vastavad read
--- prindib tulemuse konsooli
-declare @TotalCount int
-execute spGetEmployeeCountByGender 'Female', @TotalCount out
-if(@TotalCount = 0)
-	print 'TotalCount is null'
-else
-	print '@Total is not null'
-print @TotalCount
 
--- näitab ära, et mitu rid vastab nõuetele
-declare @TotalCount int
-execute spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
-print @TotalCount
+-- Loome salvestatud protseduuri töötajate arvu lugemiseks soo järgi
+create proc spGetEmployeeCountByGender
+@Gender nvarchar(20),
+@EmployeeCount int output
+as begin
+    select @EmployeeCount = count(Id) from Employees where Gender = @Gender;
+end;
 
--- sp sisu vaatamine
-sp_help spGetEmployeeCountByGender
--- tabeli info
-sp_help Employees
--- kui soovid sp tektsi näha
-sp_helptext spGetEmployeeCountByGender
+-- Käivitame protseduuri, et saada "Male" töötajate arv
+exec spGetEmployeeCountByGender 'Male';
 
--- vaatame , millest see sp sõltub
-sp_depends spGetEmployeeCountByGender
--- vaatame tabelit
-sp_depends Employees
+-- Kui soovime salvestatud protseduuri tekstilõike vaadata
+sp_helptext spGetEmployeeCountByGender;
 
+-- Protseduuride sõltuvused
+sp_depends spGetEmployeeCountByGender;
+sp_depends Employees;
 
---
+-- Näide salvestatud protseduurist, mis ei tööta õigesti
 create proc spGetnameById
 @Id int,
 @Name nvarchar(20) output
 as begin
-	select @Id = Id, @Name = FirstName from Employees
-end
+    select @Id = Id, @Name = Name from Employees;
+end;
 
-select * from Employees
-declare @FirstName nvarchar(50)
-execute spGetnameById 2, @FirstName output
-print 'Name of the employee = ' + @FirstName
-
--- mis id all on keegi nime j'rgi
+-- Näide õigest protseduurist, kus andmeid saab ID järgi
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
 as begin
-	select @FirstName = FirstName from Employees where Id = @Id
-end
+    select @FirstName = Name from Employees where Id = @Id;
+end;
 
-declare @FirstName nvarchar(50)
-execute spGetNameById1 4, @FirstName output
-print 'Name of the employee = ' + @FirstName
+-- Kuvame salvestatud protseduuride sisu ja käivitame neid
+sp_help spGetNameById1;
+declare @FirstName nvarchar(50);
+execute spGetNameById1 4, @FirstName output;
+print 'Name of the employee = ' + @FirstName;
 
-sp_help spGetNameById1
-
----
+-- Näidatakse salvestatud protseduuride sisu, kus andmed tulevad ID järgi
 create proc spGetNameById2
 @Id int
 as begin
-	return (select FirstName from Employees where Id = @Id)
-end
+    return (select Name from Employees where Id = @Id);
+end;
 
--- tuleb veateade kuna kutsusime välja int-i, aga Tom on string
-declare @FirstName nvarchar(50)
-execute @FirstName = spGetNameById2 1
-print 'Name of the employee = ' + @FirstName
---
+-- Kuvame tulemusi
+select * from Employees;
 
---- sisseehitatud string funktsioonid
--- see konverteerib ASCII tähe väärtuse numbriks
-select ascii('a')
--- kuvab A-tähe
-select char (66)
+-- Näitame ASCII tähti ja nende väärtuseid
+select ascii('a');
+select char(66); -- Kuvab "B"
 
---prindime kogu tähestiku välja
-declare @Start int
-set @Start = 97
-while (@Start <= 122)
-begin
-	select char (@Start)
-	set @Start = @Start + 1
-end
+-- Eemaldame tühikud stringi algusest ja lõpust
+select ltrim('        Hello');
+select rtrim('      Hello          ');
 
--- eemaldame tühjad kohad sulgudes
-select ltrim('        Hello')
+-- Keerame stringi ümber (reverse)
+select REVERSE(UPPER(ltrim(Name))) as FirstName, lower(Gender) as Gender, 
+rtrim(ltrim(Name)) + ' ' + Gender as FullInfo from Employees;
 
--- tühikute eemaldamine veerust
-select ltrim(FirstName) as FirstName, MiddleName, LastName from Employees
+-- Kuvame tähemärkide arvu nime järgi
+select Name, len(Name) as [Total Characters] from Employees;
 
-select * from Employees
+-- Kasutame LEFT, RIGHT ja SUBSTRING funktsioone
+select left('ABCDEF', 4);
+select right('ABCDEF', 3);
+select SUBSTRING('pam@btbb.com', 5, 2);
 
---paremalt poolt tühjad stringid lõikab ära
-select rtrim('      Hello          ')
+-- Sisestame emailide väärtuste asemel tähti
+select substring(Email, 1, 2) + REPLICATE('*', 5) + 
+SUBSTRING(Email, CHARINDEX('@', Email), len(Email) - charindex('@', Email) + 1) as Email
+from Employees;
 
---keerab kooloni sees olevad andmed vastupidiseks
--- vastavalt upper ja lower-ga saan muuta märkide suurust
--- reverse funktsioon pöörab kõik ümber
-select REVERSE(UPPER(ltrim(FirstName))) as FirstName, MiddleName, lower(LastName),
-rtrim(ltrim(FirstName)) + ' ' + MiddleName + ' ' + LastName as FullName
-from Employees
-
---näeb, mitu tähte on sõnal ja loeb tühikud sisse
-select FirstName, len(FirstName) as [Total Characters] from Employees
-
---- näeb, mitu tähte on sõnal ja ei loe tyhikuid sisse
-select FirstName, len(ltrim(FirstName)) as [Total Characters] from Employees
-
--- left, right ja substring
---- vasakult poolt neli esimest tähte
-select left('ABCDEF', 4)
--- paremalt poolt kolm tähte
-select right('ABCDEF', 3)
-
---kuvab @-tähemärgi asetust e mitmes on @ märk
-select charindex('@', 'sara@aaa.com')
-
---- esimene nr peale komakohta näitab, et mitmendast alustab ja siis mitu nr peale
--- seda kuvada
-select SUBSTRING('pam@btbb.com', 5, 2)
-
---- @-märgist kuvab kolm tähemärki. Viimase numriga saab määrata pikkust
-select substring('pam@bb.com', charindex('@', 'pam@bb.com') + 1, 3)
-
---- peale @-märki reguleerin tähemärkide pikkuse näitamist
-select substring('pam@bb.com', charindex('@', 'pam@bb.com') + 1, 
-len('pam@bb.com') - CHARINDEX('@', 'pam@bb.com'))
-
-select * from Employees
-
--- vaja teha uus veerg nimega Email, nvarchar (20)
+-- Täiendame andmebaasi, et lisada uus veerg Email
 alter table Employees
-add Email nvarchar(20)
+add Email nvarchar(20);
 
-update Employees set Email = 'Tom@aaa.com' where Id = 1
-update Employees set Email = 'Pam@bbb.com' where Id = 2
-update Employees set Email = 'John@aaa.com' where Id = 3
-update Employees set Email = 'Sam@bbb.com' where Id = 4
-update Employees set Email = 'Todd@bbb.com' where Id = 5
-update Employees set Email = 'Ben@ccc.com' where Id = 6
-update Employees set Email = 'Sara@ccc.com' where Id = 7
-update Employees set Email = 'Valarie@aaa.com' where Id = 8
-update Employees set Email = 'James@bbb.com' where Id = 9
-update Employees set Email = 'Russel@bbb.com' where Id = 10
+update Employees set Email = 'Tom@aaa.com' where Id = 1;
+update Employees set Email = 'Pam@bbb.com' where Id = 2;
+update Employees set Email = 'John@aaa.com' where Id = 3;
+update Employees set Email = 'Sam@bbb.com' where Id = 4;
+update Employees set Email = 'Todd@bbb.com' where Id = 5;
 
-select * from Employees
-
---- lisame *-märgi alates teatud kohast
-select FirstName, LastName,
-	substring(Email, 1, 2) + REPLICATE('*', 5) + --peale teist tähemärki paneb viis tärni
-	SUBSTRING(Email, CHARINDEX('@', Email), len(Email) - charindex('@', Email)+1) as Email
-from Employees
-
---- kolm korda näitab stringis olevat väärtust
-select replicate(FirstName, 3)
-from Employees
-
-select replicate('asd', 3)
-
--- kuidas sisestada tyhikut kahe nime vahele
-select space(5)
-
---Employees tabelist teed päringu kahe nime osas (FirstName ja LastName)
---kahe nime vahel on 25 tühikut
-select FirstName + space(25) + LastName as FullName
-from Employees
+-- Kuvame lõpuks kogu Employees tabeli sisu
+select * from Employees;
